1f404f76a2e19d012a560a27347326eb905570f6
MBulti <m.bulthaup.development@gmail.com>
2021-03-10 20:41:03 +0100

    some things

--- Objektorientiert/Sudoku/Program.cs
+++ Objektorientiert/Sudoku/Program.cs
@@ -6,24 +6,21 @@ namespace Sudoku_Solver
     {
         static void Main(string[] args)
         {
-            int[,] Werte = new int[,] { { 0,4,0,9,0,0,0,5,0 },
-                                        { 2,0,0,0,0,0,0,4,0 },
-                                        { 1,9,0,0,8,0,7,0,0 },
-                                        { 5,0,0,0,0,0,1,0,0 },
-                                        { 0,0,7,0,6,0,0,0,3 },
-                                        { 0,0,0,0,3,0,8,9,0 },
-                                        { 0,8,0,3,4,0,0,6,0 },
-                                        { 3,0,0,2,0,8,0,0,0 },
-                                        { 0,0,0,0,0,0,0,0,0 } };
+            int[,] sudokuEinfach = new int[,] { { 0,4,0,9,0,0,0,5,0 },
+                                                { 2,0,0,0,0,0,0,4,0 },
+                                                { 1,9,0,0,8,0,7,0,0 },
+                                                { 5,0,0,0,0,0,1,0,0 },
+                                                { 0,0,7,0,6,0,0,0,3 },
+                                                { 0,0,0,0,3,0,8,9,0 },
+                                                { 0,8,0,3,4,0,0,6,0 },
+                                                { 3,0,0,2,0,8,0,0,0 },
+                                                { 0,0,0,0,0,0,0,0,0 } };
 
-            Sudoku sudoku = new Sudoku(Werte);
-            if(sudoku.Loesen())
-            {
+            Sudoku sudoku = new Sudoku(sudokuEinfach);
+            if (sudoku.LoeseSudoku())
                 sudoku.Ausgabe();
-            }
             else
-            {
-            }
+                Console.WriteLine("Sudoku nicht lösbar.");
         }
     }
 }
--- Objektorientiert/Sudoku/Sudoku.cs
+++ Objektorientiert/Sudoku/Sudoku.cs
@@ -6,32 +6,35 @@ using System.Threading.Tasks;
 
 namespace Sudoku_Solver
 {
-    class Sudoku
+    public class Sudoku
     {
-        private int[,] Werte = new int[9, 9];
+        #region Properties
+        private const int maxLaenge = 9;
+        private int[,] spielFeld = new int[maxLaenge, maxLaenge];
+        #endregion
 
+        #region Public
         public Sudoku(int[,] Werte)
         {
-            this.Werte = Werte;
+            this.spielFeld = Werte;
         }
-
-        public bool Loesen()
+        public bool LoeseSudoku()
         {
-            for (int Zeile = 0; Zeile < Werte.GetLength(0); Zeile++)
+            for (int Zeile = 0; Zeile < spielFeld.GetLength(0); Zeile++)
             {
-                for (int Spalte = 0; Spalte < Werte.GetLength(1); Spalte++)
+                for (int Spalte = 0; Spalte < spielFeld.GetLength(1); Spalte++)
                 {
-                    if (Werte[Spalte,Zeile] == 0)
+                    if (spielFeld[Spalte,Zeile] == 0)
                     {
-                        for (int i = 1; i <= 9; i++)
+                        for (int aktuelleZahl = 1; aktuelleZahl <= 9; aktuelleZahl++)
                         {
-                            if(IstGueltig(Spalte, Zeile, i))
+                            if(IstGueltig(Spalte, Zeile, aktuelleZahl))
                             {
-                                Werte[Spalte, Zeile] = i;
-                                if (Loesen())
+                                spielFeld[Spalte, Zeile] = aktuelleZahl;
+                                if (LoeseSudoku())
                                     return true;
                                 else
-                                    Werte[Spalte, Zeile] = 0;
+                                    spielFeld[Spalte, Zeile] = 0;
                             }
                         }
                         return false;
@@ -40,36 +43,38 @@ namespace Sudoku_Solver
             }
             return true;
         }
-
         public void Ausgabe()
         {
-            for (int i = 0; i < Werte.GetLength(0); i++)
+            for (int i = 0; i < spielFeld.GetLength(0); i++)
             {
-                for (int j = 0; j < Werte.GetLength(1); j++)
+                for (int j = 0; j < spielFeld.GetLength(1); j++)
                 {
-                    Console.Write(string.Format("{0} ", Werte[i, j]));
+                    Console.Write(string.Format("{0} ", spielFeld[i, j]));
                 }
                 Console.Write("\n");
             }
             Console.ReadLine();
-        } 
+        }
+        #endregion
 
+        #region Private
         private bool IstGueltig(int Zeile, int Spalte, int Wert)
         {
-            for (int i = 0; i < 9; i++)
+            for (int i = 0; i < maxLaenge; i++)
             {
                 //check row  
-                if (Werte[i, Spalte] != 0 && Werte[i, Spalte] == Wert)
+                if (spielFeld[i, Spalte] != 0 && spielFeld[i, Spalte] == Wert)
                     return false;
                 //check column  
-                if (Werte[Zeile, i] != 0 && Werte[Zeile, i] == Wert)
+                if (spielFeld[Zeile, i] != 0 && spielFeld[Zeile, i] == Wert)
                     return false;
                 //check 3*3 block  
-                int AktuellerIndex = Werte[3 * (Zeile / 3) + i / 3, 3 * (Spalte / 3) + i % 3];
+                int AktuellerIndex = spielFeld[3 * (Zeile / 3) + i / 3, 3 * (Spalte / 3) + i % 3];
                 if (AktuellerIndex != 0 && AktuellerIndex == Wert)
                     return false;
             }
             return true;
         }
+        #endregion
     }
 }
